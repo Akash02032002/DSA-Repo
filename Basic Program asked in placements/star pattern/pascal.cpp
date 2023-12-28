c00632319a9b88b38324903cc9eb81aed275bb4c
Akash02032002 <aksahkumar02032002@gmail.com>
2023-12-29 01:33:20 +0530

    first commit

--- Basic Program asked in placements/star pattern/pascal.cpp
+++ Basic Program asked in placements/star pattern/pascal.cpp
@@ -1,24 +1,41 @@
-#include<iostream>
-using namespace std;   
-int fact(int n){
-if(n<=0) return 1; return fact(n);
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    if (n <= 0)
+        return 1;
+    return fact(n);
 }
-int combi(int n , int r ){return  (fact(n)/fact(n-r)/fact(r)); }
-void pascal(int row){
-    int k,r;
-    for (int i=1 ; i<=row ; i++){
-        k=1;r=0;
-        for(int j=1 ;j <=2*row-1 ; j++){
-            if(j>=row+1-i && j<=row-1+i && k){
-            cout<<combi(i-1,r); 
-            k=0 ;r++;}
-            else{
-            cout<< " "; k=1;}
+int combi(int n, int r) 
+  { 
+    return (fact(n) / fact(n - r) / fact(r)); 
+ }
+void pascal(int row)
+{
+    int k, r;
+    for (int i = 1; i <= row; i++)
+    {
+        k = 1;
+        r = 0;
+        for (int j = 1; j <= 2 * row - 1; j++)
+        {
+            if (j >= row + 1 - i && j <= row - 1 + i && k)
+            {
+                cout << combi(i - 1, r);
+                k = 0;
+                r++;
+            }
+            else
+            {
+                cout << " ";
+                k = 1;
+            }
         }
-        cout<<endl;
-    }}
-int main(){
-    int k,r,row;
- pascal(5);
-    
+        cout << endl;
+    }
+}
+int main()
+{
+    int k, r, row;
+    pascal(5);
 }
